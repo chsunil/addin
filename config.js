f52636d8df1b2b0f4330ff3e6daaeb22a2b7c9d6
Sunil Ch <ch.sunilchowdary@gmail.com>
2026-06-02 15:41:02 +0530

    Update

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // KEEP THIS FILE PRIVATE — do not share or commit to git
 // ============================================================
 
-window.CRM_CLIENT_SECRET = "";
-window.CRM_PASSWORD       = "";
+window.CRM_CLIENT_SECRET = "Mautic@Pigeon2026";
+window.CRM_PASSWORD       = "Sr!sys@Vedic05";
--- test.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-evens = np.arange(2, 21, 2)
-print(evens)
