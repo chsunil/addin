29956bdc1b26ac92af032c83e07b612ad8c50373
Sunil Ch <ch.sunilchowdary@gmail.com>
2026-06-02 15:11:57 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// ============================================================
-// Pigeon-Tech SuiteCRM Add-in — Credentials
-// KEEP THIS FILE PRIVATE — do not share or commit to git
-// ============================================================
-
-window.CRM_CLIENT_SECRET = "";
-window.CRM_PASSWORD       = "";
